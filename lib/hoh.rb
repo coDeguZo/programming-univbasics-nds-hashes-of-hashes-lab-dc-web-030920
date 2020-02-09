0416e726e35fd17fb776bbd7a072c63b008a392e
Nduka-Eze Ariguzo <u.ariguzo.ua@gmail.com>
2020-02-09 03:18:37 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -47,16 +47,21 @@ def naming_system
     sub_category: {
       label: "Phylum",
       sub_category: {
-        label: "Class",
-        label: "Order",
-        label: "Family",
+        label: "Class"
         sub_category: {
-          label: "Genus",
-          label: "Species",
-          sub_category: nil
+          label: "Order"
+          sub_category: {
+            label: "Family"
+            sub_category: {
+              label: "Genus"
+              sub_category: {
+                label: "Species"
+                sub_category: nil
+              }
+            }
+          }
         }
       }
     }
   }
-  hash_of_hash
 end
